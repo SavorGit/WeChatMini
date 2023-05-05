b216e0304571b209ed1e9f159a522d69e3fbdfa1
lz958942 <404644381@qq.com>
2023-05-05 16:55:47 +0800

    能不能行

--- miniprogram-ci-config.js
+++ miniprogram-ci-config.js
@@ -46,10 +46,8 @@ const project = new ci.Project({ ...reqParams });
 const previewPath = path.resolve(appDirectory, `./qrcode-${buildId}.jpg`);
 
 // 任何时候都生成二维码
-// (async () => {
-(() => {
-  // const previewResult = await ci.preview({
-  const previewResult = ci.preview({
+(async () => {
+  const previewResult = await ci.preview({
     project,
     desc: '预览', // 此备注将显示在“小程序助手”开发版列表中
     setting: uploadParams,
@@ -59,7 +57,7 @@ const previewPath = path.resolve(appDirectory, `./qrcode-${buildId}.jpg`);
     onProgressUpdate: console.log,
     // pagePath: 'pages/index/index', // 预览页面
     // searchQuery: 'a=1&b=2',  // 预览参数 [注意!]这里的`&`字符在命令行中应写成转义字符`\&`
-  }).sync();
+  });
   console.log(previewResult);
 })();
 
